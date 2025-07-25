a30c3fd6beae528e91d5ec85818b1eb5580f5fc1
klebcabailhongal <premkelageri2@gmail.com>
2025-07-25 10:41:26 +0530

    Product CRUD

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+
+const productsRoutes = require("./routes/productRoutes");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/", productsRoutes);
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+
+  .then(() => {
+    console.log("Database connected");
+    app.listen(3000, () => {
+      console.log("Server is running in port 5000");
+    });
+  })
+  .catch(() => {
+    console.log("Error connecting to database:", error.message);
+  });
